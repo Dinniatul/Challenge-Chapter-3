2c82412449d79fbdbe172f1c696dbc324a70a32d
Dinniatul <kundinnii29@gmail.com>
2022-09-06 12:41:03 +0700

    first commit

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,30 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log("sebelum ascending :");
+  console.table(cars);
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+
+  const panjang = cars.length;
+  console.log("panjang array: ", panjang);
+
+  for (let i = 0; i < panjang - 1; i++) {
+    //looping membandingan index
+    for (let j = 0; j < panjang - 1; j++) {
+      //algo bbs
+      if (result[j].year > result[j + 1].year) {
+        temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  console.log("sesudah ascending : ");
+  console.table(result);
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,28 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+
+  const panjang = cars.length;
+  console.log("panjang array: ", panjang);
+
+  for (let i = 0; i < panjang - 1; i++) {
+    //looping membandingan index
+    for (let j = 0; j < panjang - 1; j++) {
+      //algo bbs
+      if (result[j].year < result[j + 1].year) {
+        temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
